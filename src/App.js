f5dacc04077962980f28cea491f8c38ef1bb9125
sheket <sheket.inred@gmail.com>
2020-08-02 22:53:27 -0400

    Add Redux extension to store creation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import reducer from './reducers'
 import Main from './containers/Main';
 import './App.css';
 
-const store = createStore(reducer);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
 
 const App = () => (
   <Provider store={store}>
